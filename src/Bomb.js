7ce6f5a04fd1ae82b88a254ac71028db6c5f5a61
Recep Öztürk <turkishteacher23@gmail.com>
2020-03-05 20:39:05 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -5,7 +5,7 @@ export default class Bomb extends React.Component{
   constructor(props){
     super()
     this.state={
-      secondsLeft:this.props.initialCount
+      secondsLeft:props.initialCount
     }
   }
   render(){
